905155a0d36bb5c5bf38c9feed35557e26e6b7fe
Tushar Singla <tusharsingla10389@gmail.com>
2025-12-23 18:44:08 +0530

    Started learning cpp for dsa

---
+++ CppJourney/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    cout<<"enter Second Number: ";
+    cin>>b;
+    cout<<a+b;
+    return 0;
+}
