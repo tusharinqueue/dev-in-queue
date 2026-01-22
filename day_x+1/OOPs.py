4b4baac9938844a79a74706806612b33de80170e
Tushar Singla <tusharsingla10389@gmail.com>
2026-01-22 19:58:21 +0530

    class variables

--- day_x+1/OOPs.py
+++ day_x+1/OOPs.py
@@ -1,12 +1,17 @@
 #oops
 
 class Car:
+    #class variable -- defined outside constructor
+    #share data across all objects || shared among all instances of a class
+    current_year=2026
+    no_of_cars=0
     def __init__(self, color , year , for_sale , condition , company):
         self.color=color
         self.year=year
         self.for_sale=for_sale
         self.condition=condition
         self.company=company
+        Car.no_of_cars+=1 #class variable will update when a object if created
 
     def buy(self):
         print("Yes i want to buy that car")
